5bea0f80b2cfd95e4040d3ae462f15d016331554
educ8s <info@educ8s.tv>
2025-01-12 10:24:31 +0200

    video version

--- src/AnalogClock.hpp
+++ src/AnalogClock.hpp
@@ -1,24 +1,23 @@
 #pragma once
-#include "raylib.h"
- 
+#include <raylib.h>
+#include <ctime>
+
 class AnalogClock {
 
-public:
-    AnalogClock(int size, Vector2 position) : size(size), position(position), minute(0), hour(0), second(0) {};
-    void Draw();
-    void Update();
-
-private:
-    void DrawFace() const;
-    void DrawHourMarks() const;
-    void DrawHand(float handWidth, float handLength, int angle, Color color, int offset);
-    void DrawMinuteHand(int minute);
-    void DrawHourHand(int hour, int minute);
-    void DrawSecondHand(int second);
-    
-    int size;
-    Vector2 position;
-    int minute;
-    int hour;
-    int second;
+    public:
+        AnalogClock(int size, Vector2 position) :size(size), position(position), hour(0), minute(0), second(0) {};
+        void Draw() const;
+        void Update();
+    private:
+        void DrawFace() const;
+        void DrawHourMarks() const;
+        void DrawMinuteHand(int minute) const;
+        void DrawHand(float handWidth, float handLength, int angle, Color color, int offset) const;
+        void DrawHourHand(int hour, int minute) const;
+        void DrawSecondHand(int second) const;
+        int size;
+        Vector2 position;
+        int hour;
+        int minute;
+        int second;
 };
--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,29 @@
-#include "raylib.h"
+#include <raylib.h>
 #include "AnalogClock.hpp"
 
-int main() {
-    const int screenWidth = 600;
-    const int screenHeight = 600;
-    const Color LIGHT_BLUE = {225, 239, 240, 255};
-    
-    SetConfigFlags(FLAG_MSAA_4X_HINT | FLAG_WINDOW_TRANSPARENT);
+int main() 
+{
+    const int WINDOW_WIDTH = 600;
+    const int WINDOW_HEIGHT = 600;
+    Color LIGHT_BLUE = {225, 239, 240, 255};
 
-    InitWindow(screenWidth, screenHeight, "raylib Analog Clock");
-    AnalogClock analogClock = {250, {300, 300}};
+    InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Analog Clock");
     SetTargetFPS(15);
 
-    while (!WindowShouldClose()) {
+    AnalogClock clock {250, {300, 300}};
 
-        analogClock.Update();
+    //Main Loop
+    while(WindowShouldClose() == false)
+    {
+        // 1. Event Handling
 
+        // 2. Updating
+        clock.Update();
+
+        // 3. Drawing
         BeginDrawing();
-            ClearBackground(LIGHT_BLUE);
-            analogClock.Draw();
+        ClearBackground(LIGHT_BLUE);
+        clock.Draw();
         EndDrawing();
     }
 
